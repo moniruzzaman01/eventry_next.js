70d2256fb96718ba7adc0ea8f38c507e75e5fd98
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-08-18 21:17:03 +0600

    deployment check three

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -3,24 +3,24 @@ import Hero from "@/components/details/Hero";
 import Location from "@/components/details/Location";
 import { getAnEventDetails } from "@/db/quires/events";
 
-export async function generateMetadata(
-  { params: { id }, searchParams },
-  parent
-) {
-  // fetch data
-  const eventDetails = await getAnEventDetails(id);
+// export async function generateMetadata(
+//   { params: { id }, searchParams },
+//   parent
+// ) {
+//   // fetch data
+//   const eventDetails = await getAnEventDetails(id);
 
-  // optionally access and extend (rather than replace) parent metadata
-  // const previousImages = (await parent).openGraph?.images || []
+//   // optionally access and extend (rather than replace) parent metadata
+//   // const previousImages = (await parent).openGraph?.images || []
 
-  return {
-    title: `Eventry | ${eventDetails?.name}`,
-    description: eventDetails.description,
-    // openGraph: {
-    //   images: ['/some-specific-page-image.jpg', ...previousImages],
-    // },
-  };
-}
+//   return {
+//     title: `Eventry | ${eventDetails?.name}`,
+//     description: eventDetails.description,
+//     // openGraph: {
+//     //   images: ['/some-specific-page-image.jpg', ...previousImages],
+//     // },
+//   };
+// }
 
 export default async function EventDetails({ params: { id } }) {
   const eventDetails = await getAnEventDetails(id);
